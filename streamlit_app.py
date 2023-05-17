6a0e479e27c92c01e0a7fc7bf23a4cec7d6dabda
mjramos22 <133921970+mjramos22@users.noreply.github.com>
2023-05-17 16:38:35 -0600

    Update streamlit_app.py
    added more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit
 
-streamlit.title('My parents new healthy Diner')
+streamlit.title('My Mom''s new healthy Diner')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach  & Rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach  & Rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlite.text('🥑🍞 Avocado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
